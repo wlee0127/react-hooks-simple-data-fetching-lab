d817c6da7b7c30862ed328e969d71e8fcec6c0c1
william lee <will.lee.npc@gmail.com>
2024-01-01 03:49:57 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+
+import React, {useEffect, useState} from "react";
+
+function App() {
+    const [isLoaded, setIsLoaded]=useState(false);
+    const [dogImage,setDogImage]=useState();
+
+    console.log("test");
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data)
+            setDogImage(data.message);
+            setIsLoaded(true);
+        })
+    },[])
+
+    if(isLoaded===false) return <p>Loading...</p>;
+    
+
+    return (
+        <img src={dogImage} alt="A Random Dog"/>
+    )
+
+}
+
+export default App
